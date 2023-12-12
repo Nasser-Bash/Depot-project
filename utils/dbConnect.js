6dcf1d2bea59ef2fe8c79ab1b3fddc5dd612d03d
Nasser-Bash <nasseribrahim966@gmail.com>
2023-12-12 18:55:12 +0200

    add env.local file

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 export default async function dbConnect  (){
         try {
-            await mongoose.connect('mongodb+srv://nasserbash16:123456Rr@cluster0.nybimxd.mongodb.net/depot',
+            await mongoose.connect(process.env.MONGODB_URL,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
