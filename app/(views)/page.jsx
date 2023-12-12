7deb76159aa8a63ba428586658c1303592a3b31d
Nasser-Bash <nasseribrahim966@gmail.com>
2023-12-13 01:58:27 +0200

    update

--- app/(views)/page.jsx
+++ app/(views)/page.jsx
@@ -2,25 +2,25 @@ import MainContainer from "../components/layout/mainContainer";
 import HeroSection from "../components/sections/home/heroSection";
 import ProductCard from "../components/widgets/productCard";
 export default async function Home() {
-  const GetProducts = async () => {
-    try {
-      const response = await fetch(
-        `https://depot-project.vercel.app/api/products`,
-        {
-          next: {
-            revalidate: 60,
-          },
-        }
-      );
+  // const GetProducts = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       `https://depot-project.vercel.app/api/products`,
+  //       {
+  //         next: {
+  //           revalidate: 60,
+  //         },
+  //       }
+  //     );
 
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return null;
-    }
-  };
-  const products = await GetProducts();
+  //     const data = await response.json();
+  //     return data;
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //     return null;
+  //   }
+  // };
+  // const products = await GetProducts();
  
   return (
     <>
@@ -28,7 +28,7 @@ export default async function Home() {
       <MainContainer>
         <main>
           <div className="sm:grid lg:grid-cols-4 mt-10  md:grid-cols-3  sm:grid-cols-2  gap-8">
-            {products.map((product) => {
+            {/* {products.map((product) => {
               return (
                 <ProductCard
                   key={product._id}
@@ -40,7 +40,7 @@ export default async function Home() {
                   galleryimages={product.galleryimages}
                 />
               );
-            })}
+            })} */}
           </div>
         </main>
       </MainContainer>
