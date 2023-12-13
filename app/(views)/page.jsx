93fcc3eea10cef5bde96d20a27c9a71307d13fcf
Nasser-Bash <nasseribrahim966@gmail.com>
2023-12-13 19:14:36 +0200

    update

--- app/(views)/page.jsx
+++ app/(views)/page.jsx
@@ -1,41 +1,34 @@
-import dbConnect from "@/utils/dbConnect";
 import MainContainer from "../components/layout/mainContainer";
 import HeroSection from "../components/sections/home/heroSection";
 import ProductCard from "../components/widgets/productCard";
 export default async function Home() {
+  const GetProducts = async () => {
     try {
-      await  dbConnect();
-    }catch(error){
-      console.log(error);
-    }
-  // const GetProducts = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       `https://depot-project.vercel.app/api/products`,
-  //       {
-  //         next: {
-  //           revalidate: 60,
-  //         },
-  //       }
-  //     );
+      const response = await fetch(
+        `https://depot-project.vercel.app/api/products`,
+        {
+          next: {
+            revalidate: 60,
+          },
+        }
+      );
 
-  //     const data = await response.json();
-  //     return data;
-  //   } catch (error) {
-  //     console.error("Error fetching data:", error);
-  //     return null;
-  //   }
-  // };
-  // const products = await GetProducts();
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return null;
+    }
+  };
+  const products = await GetProducts();
  
   return (
-     
     <>
       <HeroSection />
       <MainContainer>
         <main>
           <div className="sm:grid lg:grid-cols-4 mt-10  md:grid-cols-3  sm:grid-cols-2  gap-8">
-            {/* {products.map((product) => {
+            {products.map((product) => {
               return (
                 <ProductCard
                   key={product._id}
@@ -47,7 +40,7 @@ export default async function Home() {
                   galleryimages={product.galleryimages}
                 />
               );
-            })} */}
+            })}
           </div>
         </main>
       </MainContainer>
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
-
-export default async function dbConnect() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected!');
-  } catch (error) {
-    console.error('Database connection error:', error);
-    throw error;
-  }
+export default async function dbConnect  (){
+        try {
+            await mongoose.connect(process.env.MONGODB_URL,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+              })
+            .then(() => console.log('Connected!'));
+        } catch (error){
+            console.log(error);
+        }
 }
